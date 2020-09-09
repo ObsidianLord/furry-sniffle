f0760197eab228e6615d6ed4d8f144599707406d
Gleb Lebedenko <RhubarbDrink@gmail.com>
2020-09-10 00:24:49 +0300

    src: add solution for problem #4

--- src/App.js
+++ src/App.js
@@ -178,6 +178,14 @@ const App = () => {
 							setOrderStatuses(nextStatuses);
 							localStorage.setItem('orderStatuses', JSON.stringify(nextStatuses));
 						}}
+						setCanceledOrder={({ itemId }) => {
+							const nextStatuses = {...orderStatuses};
+
+							nextStatuses[itemId] = 'CANCELED';
+
+							setOrderStatuses(nextStatuses);
+							localStorage.setItem('orderStatuses', JSON.stringify(nextStatuses));
+						}}
 					/>
 				</Route>
 				<Route 
